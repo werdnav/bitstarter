14faa56fcc8df03776e1ba6d87e28df4695acf4b
Andrew <silicon.components@gmail.com>
2013-07-08 10:06:19 +0000

    web.js edited

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var fileBuffer = fs.readFileSync('index.html', 'utf-8');
+var fileBuffer = new Buffer(fs.readFileSync("index.html"), "utf-8");
 
 app.get('/', function(request, response) {
   response.send(fileBuffer.toString());
