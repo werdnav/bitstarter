5e55701b5efe34c56e42c7ec23c1d52e18c6a8c3
Andrew <silicon.components@gmail.com>
2013-07-08 09:54:49 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fileBuffer = fs.readFileSync('index.html', 'utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fileBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
